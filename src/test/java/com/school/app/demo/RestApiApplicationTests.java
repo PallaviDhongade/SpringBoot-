14f4e2e923d4bebd574e7bc7ab438a9bc5a4245e
PallaviDhongade <86299156+PallaviDhongade@users.noreply.github.com>
2022-08-02 12:05:16 +0530

    Add files via upload

---
+++ src/test/java/com/school/app/demo/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.School.app.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
